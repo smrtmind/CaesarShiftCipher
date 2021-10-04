3ff0b59a7e1eaa972a15fea1e7806605c3fcbd2c
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2021-10-05 00:53:56 +0300

    fix methods

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace CaesarShiftCipher
 {
     class Program
     {
+        static Random random = new Random();
+
         static void Main(string[] args)
         {
             Console.BackgroundColor = ConsoleColor.Gray;
@@ -13,18 +15,20 @@ namespace CaesarShiftCipher
             Console.ForegroundColor = ConsoleColor.Black;
 
             string exitTheProgram = string.Empty;
-            int encryptOrDecrypt = 0;
-            int complexity = 0;
 
             while (exitTheProgram.ToLower() != "y")
             {
-                string[] text = new string[3];
+                decimal code = 098816871917;
+                decimal complexity = 0;
+                string input = string.Empty;
+                string output = string.Empty;
+                string result = string.Empty;
 
                 Console.Clear();
                 Print("[1] ENCRYPTOR\t", ConsoleColor.DarkMagenta);
                 Print("[2] DECRYPTOR\t__________\n", ConsoleColor.DarkBlue);
 
-                encryptOrDecrypt = 0;
+                int encryptOrDecrypt = 0;
                 while (encryptOrDecrypt != 1 && encryptOrDecrypt != 2)
                 {
                     Print("| ".PadLeft(33, ' '), ConsoleColor.DarkBlue);
@@ -35,56 +39,53 @@ namespace CaesarShiftCipher
 
                 if (encryptOrDecrypt == 1)
                 {
-                    text[0] = "Enter the original text:";
-                    text[1] = "Level of encryption (1-25):";
-                    text[2] = $"Encrypted:";
+                    while (input.Length == 0)
+                    {
+                        Print($"Enter the original text:\n", ConsoleColor.DarkMagenta);
+                        input = Console.ReadLine();
+                    }
+                    char[] userInput = input.ToCharArray();
+
+                    complexity = random.Next(1, 160);
+                    Print($"\nComplexity: {complexity * code}", ConsoleColor.DarkBlue);
+                    result = Algorithm(complexity, userInput, encode: true);
+                    output = "Encrypted";
                 }
 
                 if (encryptOrDecrypt == 2)
                 {
-                    text[0] = "Enter the encrypted text:";
-                    text[1] = "Level of decryption (1-25):";
-                    text[2] = "Decrypted:";
-                }
+                    while (input.Length == 0)
+                    {
+                        Print($"Enter the encrypted text:\n", ConsoleColor.DarkMagenta);
+                        input = Console.ReadLine();
+                    }
+                    char[] userInput = input.ToCharArray();
 
-                string input = string.Empty;
-                while (input.Length == 0)
-                {
-                    Print($"{text[0]} \n", ConsoleColor.DarkMagenta);
-                    input = Console.ReadLine();
-                }
-                
-                char[] userInput = input.ToLower().ToCharArray();
+                    Console.WriteLine();
+                    while (complexity > 160 || complexity == 0)
+                    {
+                        Print($"Complexity: ", ConsoleColor.DarkBlue);
+                        decimal.TryParse(Console.ReadLine(), out complexity);
+                        complexity /= code;
 
-                complexity = 0;
-                while (complexity > 25 || complexity == 0)
-                {
-                    Print($"\n{text[1]} ", ConsoleColor.DarkBlue);
-                    int.TryParse(Console.ReadLine(), out complexity);
+                        if (complexity <= 160 && complexity != 0) break;
+                    }
+                    result = Algorithm(complexity, userInput, decode: true);
+                    output = "Decrypted";
+                }           
 
-                    if (complexity <= 25 && complexity != 0) break;
-                }
-
-                if (encryptOrDecrypt == 1)
-                    Algorithm(complexity, userInput, enc: true);
-                if (encryptOrDecrypt == 2)
-                    Algorithm(complexity, userInput, dec: true);
-
-                Print($"\n{text[2]}\n", ConsoleColor.DarkMagenta);
-
-                foreach (var letter in userInput)
-                    Console.Write(letter);
-                Console.WriteLine();
+                Print($"\n{output}\n", ConsoleColor.DarkMagenta);
+                Print($"{result}\n\n");
 
                 exitTheProgram = string.Empty;
                 while (exitTheProgram.ToLower() != "n" && exitTheProgram.ToLower() != "y")
                 {
-                    Console.Write("\n\nExit the program? [y] / [n]: ");
+                    Print("Exit the program? [y] / [n]: ", ConsoleColor.DarkGray);
                     exitTheProgram = Console.ReadLine();
                 }
             }
         }
-        static void Algorithm(int complexity, char[] userInput, bool enc = false, bool dec = false)
+        static string Algorithm(decimal complexity, char[] userInput, bool encode = false, bool decode = false)
         {
             char[] symbols = GetAlphabet();
 
@@ -94,16 +95,16 @@ namespace CaesarShiftCipher
             {
                 for (int j = 0; j < symbols.Length; j++)
                 {
-                    if (enc)
+                    if (encode)
                     {
                         if (userInput[i] == symbols[j])
-                            index = j + complexity;
+                            index = j + (int)complexity;
                     }
 
-                    if (dec)
+                    if (decode)
                     {
                         if (userInput[i] == symbols[j])
-                            index = j + (symbols.Length - complexity);
+                            index = j + (symbols.Length - (int)complexity);
                     }
                 }
 
@@ -115,18 +116,26 @@ namespace CaesarShiftCipher
 
                 else userInput[i] = symbols[index];
             }
+
+            return new string(userInput);
         }
 
         public static char[] GetAlphabet()
         {
             char[] symbols = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                                'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 
-                                'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 
-                                'ъ', 'ы', 'ь', 'э', 'ю', 'я', 
-                                '.', ',', '?', '-', '/', '_', '!', ' ', '~', '+', '*', '=', '&', 
-                                '%', '#', '№', '@', ';', ':', '>', '<', '\'', '\"', '\\',
-                                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                               'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л',
+                               'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш',
+                               'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+                               'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л',
+                               'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш',
+                               'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+                               '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=',
+                               '+', '?', '<', '>', ':', ';', '.', ',', '|', '/', '~', '{', '}',
+                               '[', ']', '№', ' ', '\'', '\"','\\',
+                               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
             return symbols;
         }
