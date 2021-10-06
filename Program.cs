74141b632e7b1e9a06eadcd97c9c453dd333abb8
Oleh Hvozdetskyi <oleg.gvozdetskyi@gmail.com>
2021-10-06 17:58:52 +0300

    minor fixes

--- Program.cs
+++ Program.cs
@@ -18,11 +18,10 @@ namespace CaesarShiftCipher
 
             while (exitTheProgram.ToLower() != "y")
             {
-                decimal code = 098816871917;
                 decimal complexity = 0;
+                decimal code = 098816871917;
                 string input = string.Empty;
                 string output = string.Empty;
-                string result = string.Empty;
 
                 Console.Clear();
                 Print("[1] ENCRYPTOR\t", ConsoleColor.DarkMagenta);
@@ -44,12 +43,11 @@ namespace CaesarShiftCipher
                         Print($"Enter the original text:\n", ConsoleColor.DarkMagenta);
                         input = Console.ReadLine();
                     }
-                    char[] userInput = input.ToCharArray();
 
-                    complexity = random.Next(1, 160);
-                    Print($"\nComplexity: {complexity * code}", ConsoleColor.DarkBlue);
-                    result = Algorithm(complexity, userInput, encode: true);
-                    output = "Encrypted";
+                    complexity = random.Next(1, 161);
+                    Print($"\nKEY: {complexity * code}", ConsoleColor.DarkBlue);
+                    output = Algorithm(complexity, input, encode: true);
+                    Print($"\nEncrypted\n", ConsoleColor.DarkMagenta);
                 }
 
                 if (encryptOrDecrypt == 2)
@@ -59,23 +57,21 @@ namespace CaesarShiftCipher
                         Print($"Enter the encrypted text:\n", ConsoleColor.DarkMagenta);
                         input = Console.ReadLine();
                     }
-                    char[] userInput = input.ToCharArray();
 
                     Console.WriteLine();
-                    while (complexity > 160 || complexity == 0)
+                    while (complexity > 161 || complexity == 0)
                     {
-                        Print($"Complexity: ", ConsoleColor.DarkBlue);
+                        Print($"KEY: ", ConsoleColor.DarkBlue);
                         decimal.TryParse(Console.ReadLine(), out complexity);
                         complexity /= code;
 
-                        if (complexity <= 160 && complexity != 0) break;
+                        if (complexity <= 161 && complexity != 0) break;
                     }
-                    result = Algorithm(complexity, userInput, decode: true);
-                    output = "Decrypted";
+                    output = Algorithm(complexity, input, decode: true);
+                    Print($"\nDecrypted\n", ConsoleColor.DarkMagenta);
                 }           
 
-                Print($"\n{output}\n", ConsoleColor.DarkMagenta);
-                Print($"{result}\n\n");
+                Print($"{output}\n\n");
 
                 exitTheProgram = string.Empty;
                 while (exitTheProgram.ToLower() != "n" && exitTheProgram.ToLower() != "y")
@@ -85,10 +81,10 @@ namespace CaesarShiftCipher
                 }
             }
         }
-        static string Algorithm(decimal complexity, char[] userInput, bool encode = false, bool decode = false)
+        static string Algorithm(decimal complexity, string input, bool encode = false, bool decode = false)
         {
-            char[] symbols = GetAlphabet();
-
+            char[] userInput = input.ToCharArray();
+            char[] symbols = GetSortedArrayOfSymbols();
             int index = 0;
 
             for (int i = 0; i < userInput.Length; i++)
@@ -120,7 +116,7 @@ namespace CaesarShiftCipher
             return new string(userInput);
         }
 
-        public static char[] GetAlphabet()
+        public static char[] GetSortedArrayOfSymbols()
         {
             char[] symbols = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
                                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
@@ -146,5 +142,217 @@ namespace CaesarShiftCipher
             Console.Write(text);
             Console.ForegroundColor = ConsoleColor.Black;
         }
+
+        /*
+         * using System;
+using System.IO;
+using System.Threading;
+
+namespace CaesarShiftCipher
+{
+    class Program
+    {
+        static Random random = new Random();
+        static RandomArrayIndexes indexes = new RandomArrayIndexes();
+
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            string exitTheProgram = string.Empty;
+
+            while (exitTheProgram.ToLower() != "y")
+            {
+                decimal code = 098816871917;
+                decimal key = 0;
+                string input = string.Empty;
+                string output = string.Empty;
+                string result = string.Empty;
+                string subsequence = string.Empty;
+
+                Console.Clear();
+                Print("[1] ENCRYPTOR\t", ConsoleColor.DarkMagenta);
+                Print("[2] DECRYPTOR\t__________\n", ConsoleColor.DarkBlue);
+
+                int encryptOrDecrypt = 0;
+                while (encryptOrDecrypt != 1 && encryptOrDecrypt != 2)
+                {
+                    Print("| ".PadLeft(33, ' '), ConsoleColor.DarkBlue);
+                    Print("select: ");
+                    int.TryParse(Console.ReadLine(), out encryptOrDecrypt);
+                }
+                Console.WriteLine();
+
+                if (encryptOrDecrypt == 1)
+                {
+                    while (input.Length == 0)
+                    {
+                        Print($"Enter the original text:\n", ConsoleColor.DarkMagenta);
+                        input = Console.ReadLine();
+                    }
+                    char[] userInput = input.ToCharArray();
+
+                    key = random.Next(1, 161);
+                    result = Encode(key, userInput);
+                    Print($"\nKEY: {key * code} / ", ConsoleColor.DarkBlue);
+
+                    foreach (var index in indexes.Indexes)
+                        Print($"{index} ", ConsoleColor.DarkBlue);
+
+                    output = "Encrypted";
+                }
+
+                if (encryptOrDecrypt == 2)
+                {
+                    while (input.Length == 0)
+                    {
+                        Print($"Enter the encrypted text:\n", ConsoleColor.DarkMagenta);
+                        input = Console.ReadLine();
+                    }
+                    char[] userInput = input.ToCharArray();
+
+                    Console.WriteLine();
+                    while (key > 161 || key == 0)
+                    {
+                        Print($"KEY: ", ConsoleColor.DarkBlue);
+                        decimal.TryParse(Console.ReadLine(), out key);
+                        Print("/");
+                        subsequence = Console.ReadLine();
+                        key /= code;
+
+                        if (key <= 161 && key != 0) break;
+                    }
+                    result = Decode(key, subsequence, userInput);
+                    output = "Decrypted";
+                }
+
+                Print($"\n{output}\n", ConsoleColor.DarkMagenta);
+                Print($"{result}\n\n");
+
+                exitTheProgram = string.Empty;
+                while (exitTheProgram.ToLower() != "n" && exitTheProgram.ToLower() != "y")
+                {
+                    Print("Exit the program? [y] / [n]: ", ConsoleColor.DarkGray);
+                    exitTheProgram = Console.ReadLine();
+                }
+            }
+        }
+        static string Encode(decimal complexity, char[] userInput)
+        {
+            char[] randomArray = GetRandomArray();
+
+            int index = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                for (int j = 0; j < randomArray.Length; j++)
+                {
+                    if (userInput[i] == randomArray[j])
+                        index = j + (int)complexity;
+                }
+
+                if (index >= randomArray.Length)
+                {
+                    index -= randomArray.Length;
+                    userInput[i] = randomArray[index];
+                }
+
+                else userInput[i] = randomArray[index];
+            }
+
+            return new string(userInput);
+        }
+
+        static string Decode(decimal complexity, string subsequence, char[] userInput)
+        {
+            char[] sortedArray = GetSortedArray();
+            string[] sub = subsequence.Split("");
+            int[] indexes = new int[sub.Length];
+
+            for (int i = 0; i < sub.Length; i++)
+                indexes[i] = Convert.ToInt32(sub[i]);
+
+            char[] randomArray = new char[sortedArray.Length];
+            for (int i = 0; i < randomArray.Length; i++)
+                randomArray[i] = sortedArray[indexes[i]];
+
+            int index = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                for (int j = 0; j < randomArray.Length; j++)
+                {
+                    if (userInput[i] == randomArray[j])
+                        index = j + (randomArray.Length - (int)complexity);
+                }
+
+                if (index >= randomArray.Length)
+                {
+                    index -= randomArray.Length;
+                    userInput[i] = randomArray[index];
+                }
+
+                else userInput[i] = randomArray[index];
+            }
+
+            return new string(userInput);
+        }
+
+        public static char[] GetSortedArray()
+        {
+            //161 symbols
+            char[] sortedArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                                   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                   'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                                   'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л',
+                                   'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш',
+                                   'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+                                   'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л',
+                                   'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш',
+                                   'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+                                   '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=',
+                                   '+', '?', '<', '>', ':', ';', '.', ',', '|', '/', '~', '{', '}',
+                                   '[', ']', '№', ' ', '\'', '\"','\\',
+                                   '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            return sortedArray;
+        }
+
+        public static char[] GetRandomArray()
+        {
+            char[] sortedArray = GetSortedArray();
+            char[] randomArray = new char[sortedArray.Length];
+            int[] randomArrayIndexes = new int[randomArray.Length];
+
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                int temp = random.Next(0, 161);
+
+                if (randomArray[i] == sortedArray[temp]) continue;
+                else
+                {
+                    randomArray[i] = sortedArray[temp];
+                    randomArrayIndexes[i] = temp;
+                }
+            }
+
+            indexes = new RandomArrayIndexes(randomArrayIndexes);
+
+            return randomArray;
+        }
+
+        public static void Print(string text, ConsoleColor color = ConsoleColor.Black)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
+
+         */
     }
 }
---
+++ RandomArrayIndexes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarShiftCipher
+{
+    public class RandomArrayIndexes
+    {
+        public int[] Indexes { get; set; }
+
+        public RandomArrayIndexes() { }
+
+        public RandomArrayIndexes(int[] indexes) => Indexes = indexes;
+    }
+}
